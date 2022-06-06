ffc294adb7d5df65d4de94629c3c8cedccd93f2c
wenjie <jinwenjie@live.com>
2022-06-06 11:53:25 +0800

    feat: add -p --push tag to run git push after committing

--- bin/commit.js
+++ bin/commit.js
@@ -13,6 +13,7 @@ program
   .name('commit')
   .version(pkg.version)
   .description('A git commit cli help git users to write git commit messages easily.')
+  .option('-p, --push', 'run git pull -r and git push after committing', false)
   .option('-s, --staged', 'only commit staged files', false)
   .option('-d, --deep <length>', 'length of cards for selecting', commandParseInt, 5)
   .action(async () => {
